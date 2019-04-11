cc40235453bdf46f3451fde8cd671705d35251ef
Vitaliy Hramchenko <v.hramchenko@gmail.com>
2019-04-11 19:19:16 +0500

    Create ArtificialHandwritting.py

---
+++ ArtificialHandwritting.py
@@ -0,0 +1,164 @@
+import os
+import cv2
+from xml.dom import minidom
+import matplotlib.pyplot as plt
+from math import floor
+from random import random
+import scipy as sp
+
+
+class ArtificialHandwrittingObject:
+    
+    def __init__(self, name, img):
+        self.name = name
+        self.image = img
+
+class ArtificialHandwritting:
+    
+    def __init__(self, datasetDir, scale, image_width, image_height, encode_word):
+        self.scale = scale
+        self.height = image_height
+        self.width = image_width
+        self.datasetDirectory = datasetDir
+        self.data = {}
+        self.initOffsets()
+        self.encode_word = encode_word
+        
+        for f_name in os.listdir(datasetDir):
+            if not f_name.endswith(".xml"):
+                continue
+            full_path = datasetDir + f_name
+            print(full_path)
+            self.process_xml(full_path)
+            
+    def initOffsets(self):
+        self.offsets = {
+            'a': 0,
+            'b': -0.1,
+            'c': 0,
+            'd': 0,
+            'e': 0,
+            'f': -0.2,
+            'g': +0.3,
+            'h': 0,
+            'i': -0.15,
+            'j': +0.2,
+            'k': 0,
+            'l': -0.15,
+            'm': 0,
+            'n': 0,
+            'o': 0,
+            'p': 0,
+            'q': +0.2,
+            'r': 0,
+            's': 0,
+            't': -0.2,
+            'u': 0,
+            'v': 0,
+            'w': 0,
+            'x': 0,
+            'y': +0.2,
+            'z': 0,
+        }
+        
+        
+        
+    def getValue(self, node, name):
+        v  = node.getElementsByTagName(name) 
+        if len(v) != 1:
+            raise Exception()
+        v = v[0].firstChild.data
+        return v
+    
+    def make_random_batch(self, word_len, rand_x):
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        texts = []
+        datas = []
+        #data = torch.FloatTensor(batch_size, self.image_height, self.image_width)
+        for batch_idx in range(0, batch_size):
+          w = ""
+          for i in range(0, word_len):
+            s_idx = floor(random()*len(alphabet))
+            s = alphabet[s_idx]
+            w += s
+          x = floor(random()*rand_x) + 10
+          d, _ = self.make_word(w, x)
+          d = d = torch.as_tensor(d)#.unsqueeze(0)
+          
+          datas.append(d)
+          w = self.encode_word(w)
+          texts.append(w)
+        t = torch.stack(texts)
+        data = torch.stack(datas, dim=0)
+        return data, t
+          
+       
+    def make_word(self, word, x_start):
+        center = self.height*0.5
+        result = sp.ones([self.height, self.width], dtype="float32")
+        candidates = []
+        for k in self.data.keys():
+            d = self.data[k]
+            flag = True
+            for s in word:
+                if s not in d:
+                    flag = False
+                    break
+            if flag:
+                candidates.append(k)
+        idx = floor(random()*len(candidates))
+        if (len(candidates) == 0):
+          print("Bad word: " + word)
+          raise Exception()
+        
+        img_file = candidates[idx]
+        page = self.data[img_file]
+        x = x_start
+        for s in word:
+            s_idx = floor(len(page[s])*random())
+            obj_im = page[s][s_idx].image
+            c = 0.5*obj_im.shape[0]
+            dy = 0 + floor(center - c + obj_im.shape[0]*(self.offsets[s]+random()*0.05))#+random()*0.05))
+            #print(dy)
+            #print(obj_im.dtype)
+            result[dy: dy + obj_im.shape[0], x: x+obj_im.shape[1]] = obj_im
+            x += obj_im.shape[1]
+        return result, img_file  
+      
+            
+    def process_xml(self, f_path):
+        
+        xml_file = minidom.parse(f_path)
+        #xml_file.normalize()
+        annotation = xml_file.getElementsByTagName('annotation')
+        if len(annotation) != 1:
+            raise Exception()
+        annotation = annotation[0]
+        img_name = self.getValue(annotation, 'filename')
+        image_file = self.datasetDirectory + img_name
+        img = cv2.imread(image_file, 0)
+        print(image_file)
+        objects = annotation.getElementsByTagName('object')
+        #print(len(objects))
+        content = {}
+        for obj in objects:
+            name  = self.getValue(obj, 'name')
+            if name not in content:
+                content[name] = []
+            x_min = int(self.getValue(obj, 'xmin'))
+            y_min = int(self.getValue(obj, 'ymin'))
+            x_max = int(self.getValue(obj, 'xmax'))
+            y_max = int(self.getValue(obj, 'ymax'))
+            obj_im = img[y_min: y_max, x_min: x_max]
+            s = self.scale
+            sh = obj_im.shape
+            obj_im = cv2.resize(obj_im, (floor(sh[1]*s), floor(sh[0]*s)), cv2.INTER_LANCZOS4)
+            if obj_im.shape[0] > self.height:
+                continue
+            obj_im = sp.float32(obj_im)    
+            obj_im = (obj_im - obj_im.min())/(obj_im.max() - obj_im.min())
+            handwr_obj = ArtificialHandwrittingObject(name, obj_im)
+            content[name].append(handwr_obj)
+        if img_name in self.data:
+            raise Exception()
+        self.data[img_name] = content
